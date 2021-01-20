3ead7a3bb26c9ee36cd60c39274326b3562643cd
johnmuthua <caveomuthama@gmail.com>
2021-01-20 17:12:28 +0300

    finished the question footer

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Container, Row, Col, Badge } from "reactstrap";
-import {BinocularsFill, BoxArrowInUp, Archive, ChatQuote, EmojiFrown} from 'react-bootstrap-icons'
-import {} from './Question.module.css';
+import {BinocularsFill, BoxArrowInUp, Archive, ChatQuote, EmojiFrown, ChatRightQuote, Star} from 'react-bootstrap-icons'
+import styles from './Question.module.css';
 
 const Question = () => {
   return (
-    <Container className={StyleSheet.card}>
+    <div className={styles.question_cover}>
       <Row>
         <Col xs="12" md="8">
           <Row>
-            <Col xs="4">
+            <Col xs="4" className={styles.question_attr}>
+            <span><small><Badge color="info">Beginner</Badge></small></span>
                   <span>
                       <BinocularsFill /> Views 10
                   </span>
@@ -26,19 +27,31 @@ const Question = () => {
                       <EmojiFrown /> status Closed
                   </span>
             </Col>
-            <Col xs="8">
-                <h1>Title</h1> <small>Posted 12hrs ago</small>
-                <h4> Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            <Col className={styles.question_main_card}>
+                <div className={styles.question_main_title}>
+                <h4>Title</h4> <small>Posted 12hrs ago</small>
+                </div>
+                <div className={styles.question_main}>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
                     Repellat aut voluptatem, incidunt placeat totam error iure 
                     eveniet repudiandae. Voluptatem quisquam quis similique distinctio
-                     at aliquam reprehenderit commodi consequuntur pariatur iste!</h4>
-                <h5>Heading <Badge color="info">Beginner</Badge></h5>
+                     at aliquam reprehenderit commodi consequuntur pariatur iste!
+            
+                </div>
+                <hr />
+                <div className={styles.question_footer_main}>
+                    <span>
+                        <small><ChatRightQuote /> Comment</small>
+                    </span>
+                    <span>
+                        <small><Star /> Like</small>
+                    </span>
+                </div>
             </Col>
           </Row>
         </Col>
-        <Col>Asside</Col>
       </Row>
-    </Container>
+    </div>
   );
 };
 
